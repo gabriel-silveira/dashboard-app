f94d59d68c3a1c6f7c2e48893af725cf67ebc68d
Gabriel Silveira <gabriel@gabrielsilveira.com.br>
2025-03-10 14:04:42 -0300

    Handling errors

--- app/lib/actions/invoices-actions.ts
+++ app/lib/actions/invoices-actions.ts
@@ -20,42 +20,56 @@ const CreateInvoiceSchema = FormSchema.omit({id: true, date: true});
 const UpdateInvoiceSchema = FormSchema.omit({id: true, date: true});
 
 export async function createInvoice(formData: FormData) {
-  const {customerId, amount, status} = CreateInvoiceSchema.parse(
-    Object.fromEntries(new Map([...formData])),
-  );
+  try {
+    const {customerId, amount, status} = CreateInvoiceSchema.parse(
+      Object.fromEntries(new Map([...formData])),
+    );
 
-  const amountInCents = amount * 100;
+    const amountInCents = amount * 100;
 
-  const date = new Date().toISOString().split('T')[0];
+    const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    console.error(error);
+  }
 
   redirectToInvoices();
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const {customerId, amount, status} = UpdateInvoiceSchema.parse(
-    Object.fromEntries(new Map([...formData])),
-  );
+  try {
+    const {customerId, amount, status} = UpdateInvoiceSchema.parse(
+      Object.fromEntries(new Map([...formData])),
+    );
 
-  const amountInCents = amount * 100;
+    const amountInCents = amount * 100;
 
-  await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId},
-          amount      = ${amountInCents},
-          status      = ${status}
-      WHERE id = ${id}
-  `
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId},
+            amount      = ${amountInCents},
+            status      = ${status}
+        WHERE id = ${id}
+    `
+  } catch (error) {
+    console.error(error);
+  }
 
   redirectToInvoices();
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
+    await sql`DELETE
+              FROM invoices
+              WHERE id = ${id}`;
+  } catch (error) {
+    console.error(error);
+  }
 
   revalidatePath('/dashboard/invoices');
 }
