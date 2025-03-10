503127e87cbf8c278bde03cb559b8b19c455df01
Gabriel Silveira <gabriel@gabrielsilveira.com.br>
2025-03-10 18:33:03 -0300

    Improving accessibility (WIP)

--- app/lib/actions/invoices-actions.ts
+++ app/lib/actions/invoices-actions.ts
@@ -5,13 +5,22 @@ import postgres from "postgres";
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({message: 'Please select a customer.'}),
+  amount: z.coerce.number().gt(0, {message: 'Please enter an amount greater than $0.'}),
+  status: z.enum(['pending', 'paid'], {message: 'Please select an invoice status.'}),
   date: z.string(),
 });
 
@@ -19,22 +28,34 @@ const CreateInvoiceSchema = FormSchema.omit({id: true, date: true});
 
 const UpdateInvoiceSchema = FormSchema.omit({id: true, date: true});
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoiceSchema.safeParse(
+    Object.fromEntries(new Map([...formData])),
+  );
+
+  // if form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  const {customerId, amount, status} = validatedFields.data;
+
+  const amountInCents = amount * 100;
+
+  const date = new Date().toISOString().split('T')[0];
+
   try {
-    const {customerId, amount, status} = CreateInvoiceSchema.parse(
-      Object.fromEntries(new Map([...formData])),
-    );
-
-    const amountInCents = amount * 100;
-
-    const date = new Date().toISOString().split('T')[0];
-
     await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
-    console.error(error);
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
   }
 
   redirectToInvoices();
